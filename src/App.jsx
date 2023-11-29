64d0ec715fa396b99bb40f29f59efbe1b3e25c11
kabyl-ulan <kabylulanabdykalykuulu@gmail.com>
2023-11-29 13:57:34 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//local
+import ContributionGraph from "./components/ContributionGraph";
+
+function App() {
+  return (
+    <>
+      <ContributionGraph />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ContributionGraph/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ContributionGraph() {
+  return <>ContributionGraph</>;
+}
+
+export default ContributionGraph;
